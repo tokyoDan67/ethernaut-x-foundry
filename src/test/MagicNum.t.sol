5a5ffeaa0b835e6b82c0ace42af0afd89dbd7f2d
tokyoDan67 <danielyamagata12@gmail.com>
2023-06-19 21:59:58 -0400

    Passed MagicNum

--- src/test/MagicNum.t.sol
+++ src/test/MagicNum.t.sol
@@ -30,17 +30,48 @@ contract MagicNumTest is DSTest {
         // LEVEL ATTACK //
         //////////////////
 
-        // INIT CODE
+        // Key:
+        // PUSH2 = 
+        // CODECOPY = 39
 
-        bytes memory code = "";
+        // INIT CODE
+        // Copy the runtime code to memory...
+
+        // PUSH1 0A
+        // DUP1
+        // PUSH1 ** Insert where the runtime starts **
+        // PUSH1 00
+        // CODECOPY
+        // Return the runtime code...
+        // PUSH1 00 
+        // RETURN 
+        // STOP 
+
+        // "\x60\x0A\x80\x60\x0C\x60\x00\x39\x60\x00\xF3\x00\x60\x42\x60\x80\x52\x60\x80\x60\x01\xF3"
+
+        // RUNTIME CODE
+        // Store 42 at the free memory pointer...
+        // PUSH1 42
+        // PUSH1 80
+        // MSTORE 
+        // Return 42...
+        // PUSH1 01
+        // PUSH1 80
+        // RETURN
+        // "\x60\x42\x60\x80\x52\x60\x20\x60\x80\xF3"
+
+
+        bytes memory code = "\x60\x0A\x80\x60\x0C\x60\x00\x39\x60\x00\xF3\x00\x60\x2A\x60\x80\x52\x60\x20\x60\x80\xF3";
         address addr;
         assembly {
+          // First 32 are save for size ?
           addr := create(0, add(code, 0x20), mload(code))
           if iszero(extcodesize(addr)) {
             revert(0, 0)
           }
         }
         ethernautMagicNum.setSolver(addr);
+
 
         //////////////////////
         // LEVEL SUBMISSION //
